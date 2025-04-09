11e31f1ebfd53bde9a39e2590dee20f5e11a05f9
瞿宝明 <qubaoming.qbm@bytedance.com>
2025-04-09 18:01:10 +0800

    fix: api key

--- dist/index.js
+++ dist/index.js
@@ -35,10 +35,11 @@ const server = new Server({
 /**
  * API 基础地址
  */
-if (!process.env.API_BASE_URL) {
+const API_BASE_URL = process.env.API_BASE_URL;
+// const API_BASE_URL = 'http://localhost:9000';
+if (!API_BASE_URL) {
     throw new Error('API_BASE_URL environment variable is not defined');
 }
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:9000';
 /**
  * 组件库文档数据存储
  */
